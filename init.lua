6bcef65d89b415a19388e22fa7a23a8f7e2b6bab
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-09-11 13:49:22 +0100

    feat: update
    - Simple declarative plugin spec to define build commands with standard
      specs
    - Helper functions to shorten plugin repo urls
    - Switch to emmylua_ls (instead of lua_ls)

--- init.lua
+++ init.lua
@@ -37,7 +37,6 @@ vim.o.signcolumn      = "yes"
 vim.o.statuscolumn    = "%=%{v:relnum == 0 ? v:lnum : v:relnum} %s"
 
 
-
 vim.o.list            = true      -- show invisible characters (e.g. trailing spaces)
 
 vim.o.exrc            = true
@@ -47,34 +46,103 @@ vim.o.wrap            = false
 
 -- 2. PLUGIN INSTALLATION ==============================================================================================
 
-vim.api.nvim_create_autocmd("PackChanged", {
-  callback = function(ev)
-    if ev.data.spec.name == "nvim-treesitter" and ev.data.kind == "update" --[[or ev.data.kind == "install"]] then
-      local ok, _ = pcall(function() require "nvim-treesitter" .update "all" end)
-      if not ok then vim.notify "[ERROR] Failed to update nvim-treesitter parsers" end
-    end
-  end,
-})
+-- TODO: Make sure sync and async plugin installation works.
+-- Sync is requried for bootstrap/headless scripts.
 
-vim.pack.add {
+local build_group = vim.api.nvim_create_augroup("tiny.pack.build", {})
+
+---Set build commands for plugins. These are commands that should be run after a plugin is installed or updated.
+---@param name string Plugin name.
+---@param fn fun() Build command.
+local function build(name, fn)
+  vim.api.nvim_create_autocmd("PackChanged", {
+    once = true,
+    group = build_group,
+    desc = name,
+    callback = function(ev)
+      local kind = ev.data.kind
+      -- Delete autocmd on wrong event kind
+      if kind ~= "install" and kind ~= "update" then return true end
+      -- Keep autocmd if we have the wrong name -- it might run on a later plugin
+      if name ~= ev.data.spec.name then return false end
+
+      -- Ensure plugin is loaded.
+      if not ev.data.active then
+        vim.cmd.packadd(name)
+      end
+
+      fn()
+
+      -- Delete autocmd when done.
+      return true
+    end
+  })
+end
+
+---Extended plugin spec.
+---@class TinyPluginSpec : vim.pack.Spec
+---@field build fun() Build command.
+
+-- Common URL shorteners
+local gh = function(x) return "https://github.com/" .. x end
+local gl = function(x) return "https://gitlab.com/" .. x end
+local cb = function(x) return "https://codeberg.org/" .. x end
+
+---@type (string|TinyPluginSpec)[]
+local plugins = {
   -- appearance
-  "https://github.com/rebelot/kanagawa.nvim",
+  gh "rebelot/kanagawa.nvim",
   -- "https://github.com/echasnovski/mini.icons",
 
   -- navigation
-  "https://github.com/stevearc/oil.nvim",
+  gh "stevearc/oil.nvim",
   -- "https://github.com/ibhagwan/fzf-lua",
 
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-  "https://github.com/neovim/nvim-lspconfig", -- data only
+  {
+    src = gh "nvim-treesitter/nvim-treesitter",
+    version = "main",
+    build = function()
+      local ok, _ = pcall(function() require "nvim-treesitter" .update "all" end)
+      if not ok then vim.notify "[ERROR] Failed to update nvim-treesitter parsers" end
+    end
+  },
+
+  gh "neovim/nvim-lspconfig", -- data only
 }
 
+-- Prepare build commands before plugin installation
+for _, p in ipairs(plugins) do
+  if p.build then
+    -- TODO: nil check?
+    local name = p.name or p.src:match "/([^/]+)$"
+    build(name, p.build)
+  end
+end
+
+-- TODO: Maybe explicitly remove `build` field from specs?
+-- For now this is not an issue as it is ignored.
+vim.pack.add(plugins)
+
+-- -- After plugin stuff is done, if there are any autocmds that haven't been run, log it and then clear the group.
+-- -- This may not be desired, as you may update a plugin later in a session?? Need to investigate this.
+--
+-- -- Get desc/name of build autocmds that did not run.
+-- local autocmds = vim.iter(vim.api.nvim_get_autocmds({ group = build_group }))
+--   :map(function(a) return a.desc or "unknown" end)
+--   :totable()
+--
+-- -- Pritn autocmds that did not run
+-- vim.print(autocmds)
+--
+-- -- Delete autocmds that did not run
+-- vim.api.nvim_del_augroup_by_id(build_group)
 
 -- 3. PLUGIN SETUP =====================================================================================================
 
 -- 3.a. kanagawa.nvim (colorscheme) ------------------------------------------------------------------------------------
 
-require "kanagawa" .setup { ---@type KanagawaConfig
+---@diagnostic disable-next-line
+require "kanagawa" .setup {
   colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
 
   ---@param colors KanagawaColors
@@ -96,6 +164,7 @@ vim.cmd.colorscheme "kanagawa"
 
 -- 3.b. oil.nvim (file explorer) ---------------------------------------------------------------------------------------
 
+---@diagnostic disable-next-line
 require "oil" .setup {
   columns = {
     "icon",
@@ -146,12 +215,48 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- 4. LSP ==============================================================================================================
 
-vim.lsp.enable {
-  "lua_ls",
-}
+-- If there is a config file, use that. Otherwise, assume neovim config.
+-- TODO: Perhaps add a mechanism to opt-in to the Neovim config, e.g. an
+-- environment variable, file marker etc. Then in cases where there is no
+-- config file and no opt-in, nothing will be used.
+vim.lsp.config("emmylua_ls", {
+  on_init = function(client)
+    -- If the workspace has its own emmylua_ls/lua_ls config file, defer to it.
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath("config")
+        and (vim.uv.fs_stat(path .. "/.emmyrc.json")
+        or vim.uv.fs_stat(path .. "/.luarc.json"))
+      then
+        client.config.settings = {}
+      end
+    end
+  end,
+  settings = {
+    emmylua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      -- Make the server aware of Neovim runtime files.
+      workspace = {
+        -- library = {
+        --   vim.env.VIMRUNTIME,
+        -- },
+        -- Or pull in all of 'runtimepath'. May be slower!
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    },
+  },
+})
+
+vim.lsp.enable { "emmylua_ls" }
 
 
 -- 5. KEYMAPS ==========================================================================================================
+
+-- TODO: The default <C-l> is useful, especially now we have multicursor.
+-- Need to come up with a new keymap for that type of thing.
+-- See `:h CTRL-L-default`.
 
 -- 5.a. General --------------------------------------------------------------------------------------------------------
 
@@ -171,7 +276,6 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left pane" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus lower pane" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus upper pane" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right pane" })
-
 
 -- 5.c. Open Splits ----------------------------------------------------------------------------------------------------
 
