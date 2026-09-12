4c6cc9f79ab44b971e6812e618503e22b64a5e6a
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-09-13 00:36:59 +0100

    feat(oil): do not show hidden files by default

--- init.lua
+++ init.lua
@@ -144,9 +144,6 @@ require "oil" .setup {
     "size",
     "mtime",
   },
-  view_options = {
-    show_hidden = true,
-  },
   -- Skip the confirmation popup for simple operations (:h oil.skip_confirm_for_simple_edits)
   skip_confirm_for_simple_edits = true,
   float = {
